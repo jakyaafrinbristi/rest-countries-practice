7d7f422ae94b830d358dfe7ad2ab8b90dab93dcf
jakyaafrinbristi <jakyaafrinb@gmail.com>
2023-12-09 13:13:01 +0600

    showSingleCountry

--- js/countries.js
+++ js/countries.js
@@ -4,7 +4,13 @@ const loadCountries = () => {
     .then(res => res.json())
     .then(data => showData(data.slice(0, 5)))
 }
-loadCountries()
+const showAllCountries = () => {
+  fetch(`
+  https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then(data => showData(data))
+}
+
 
 const showData = (countries) => {
   const cardContainer = document.getElementById('country-info');
@@ -23,14 +29,50 @@ const showData = (countries) => {
       <p class="text-xl">Population:${country.population}</p>
       <p class="text-xl">Currency:${country.currencies ? Object.keys(country.currencies)[0] : "Not Found"
       }</p>
-      
-  
+
       <div class="card-actions justify-end">
-      <div class="btn btn-primary"onClick=>Details</div>
-      </div>
+      <label onClick="getDetailsData('${country.cca2}')" for="my_modal_6" class="btn">Details</label>
+    </div>
+      
     </div>
   </div>`
 cardContainer.appendChild(card);
   });
 
 }
+const getDetailsData=(id)=>{
+  fetch(`
+  https://restcountries.com/v3.1/name/${id}`)
+    .then(res => res.json())
+    .then(data => showSingleCountryData(data[0]))
+}
+
+
+  const modalContainer=document.getElementById('modal-body');
+  modalContainer.innerHTML=''
+  const showSingleCountryData=(singleCountryData)=>{
+    console.log(singleCountryData)
+    const div=document.createElement('div');
+    div.classList.add("modal-box");
+    div.innerHTML=`
+    <img class="w-full h-40"  src="${singleCountryData.flags.png}" alt="Shoes" />
+    <div class="card-body">
+      <h2 class="card-title">
+        ${singleCountryData.name.common}
+        
+      </h2>
+      <p class="text-xl">  Capital: ${singleCountryData?.capital ? singleCountryData?.capital[0]: "Not Found"}</p>
+      <p class="text-xl">Population:${singleCountryData.population}</p>
+      <p class="text-xl">Currency:${singleCountryData.currencies ? Object.keys(singleCountryData.currencies)[0] : "Not Found"
+      }</p>
+    <div class="modal-action">
+        <label for="my_modal_6" class="btn">Close!</label>
+    </div>
+    </div>`
+    modalContainer.appendChild(div)
+
+
+
+
+}
+loadCountries()
